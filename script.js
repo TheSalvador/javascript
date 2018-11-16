be9b192888cd275118cd104c900398255721d633
TheSalvador <45077212+TheSalvador@users.noreply.github.com>
2018-11-16 21:47:51 +0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,18 +1,22 @@
 'use strict';
 
-let money = prompt('Ваш бюджет на месяц?'),
-    time = prompt('Введите дату в формате YYYY-MM-DD'),
-    answer = prompt('Введите обязательную статью расходов в этом месяце'),
-    answer1 = prompt('Во сколько обойдется?');
+let money   = prompt('Ваш бюджет на месяц?'),
+    time    = prompt('Введите дату в формате YYYY-MM-DD'),
+    answer  = prompt('Введите обязательную статью расходов в этом месяце'),
+    answer1 = prompt('Введите обязательную статью расходов в этом месяце'),
+    answer2 = prompt('Во сколько обойдется?'),
+    answer3 = prompt('Во сколько обойдется?');
 console.log(money, time);
 
 let appData = {
     budget: money,
     timeData: time,
-    expenses:{ answer, answer1
+    expenses:{ 
+      answer : answer2,
+      answer1 : answer3,
     },
-    optionalExpenses: '',
-    income: '',
+    optionalExpenses: {},
+    income: [],
     savings: false,
   };
 console.log(appData);
