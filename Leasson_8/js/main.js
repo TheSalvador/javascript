fe4ee7fe610209fc0b0fe18cfa76d848a817a89b
TheSalvador <45077212+TheSalvador@users.noreply.github.com>
2018-11-29 21:45:38 +0300

    Add files via upload

--- Leasson_8/js/main.js
+++ Leasson_8/js/main.js
@@ -33,12 +33,12 @@ window.addEventListener('DOMContentLoaded', function() {
 		}
 	});
 //Timer
-	// let deadLine = '2018-11-29';
+	let deadLine = "2018.11.28";
 	function getTimeRemaining(endtime) {
 		let t = Date.parse(endtime) - Date.parse(new Date()),
 			second = Math.floor((t/1000) % 60),
 			minutes = Math.floor((t/1000/60) % 60),
-			hour = Math.floor((t/(100*60*60))%24);
+			hour = Math.floor((t/(1000*60*60)));
 			
 			return {
 				'total' : t,
@@ -47,25 +47,28 @@ window.addEventListener('DOMContentLoaded', function() {
 				'seconds' : second
 			};
 	}
-
 	function setClock(id, endtime) {
 		let timer = document.getElementById(id),
 			hour = timer.querySelector('.hours'),
 			minutes = timer.querySelector('.minutes'),
-			second = timer.querySelector('.seconds');
+			second = timer.querySelector('.seconds'),
 			timeInterval = setInterval(updateClock, 1000);
 
 		function updateClock() {
 			let t = getTimeRemaining(endtime);
-			hour.innerHTML = ('0' + t.hours).slice(-2),
-			minutes.innerHTML = ('0' + t.minutes).slice(-2),
-			second.innerHTML = ('0' +  t.seconds).slice(-2);
-			if (t.total <=0) {
-				clearInterval(timeInterval);
+					hour.innerHTML = ('0' + t.hours).slice(-2);
+					minutes.innerHTML = ('0' + t.minutes).slice(-2);
+					second.innerHTML = ('0' +  t.seconds).slice(-2);
+					if (t.total <=0) {
+						clearInterval(timeInterval);
+						document.querySelector('.hours').innerHTML = '00';
+						document.querySelector('.minutes').innerHTML = '00';
+						document.querySelector('.seconds').innerHTML = '00';
 			}
 		}
 		updateClock();
 	}
-	let deadLine = new Date(Date.parse(new Date())+15*60*60*1000);
+	// let deadLine = new Date(Date.parse(new Date())+15*60*60*1000);
+		// let deadLine = "2018.11.28";
 	setClock('timer', deadLine);
 });
