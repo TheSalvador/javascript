7a27fd82739cd7e271d6d3ead6b35f553759b681
TheSalvador <45077212+TheSalvador@users.noreply.github.com>
2018-12-15 21:29:14 +0300

    Add files via upload

--- Leassons1/js/form.js
+++ Leassons1/js/form.js
@@ -54,6 +54,20 @@ closeBtn = addEventListener("click", function (event) {
         document.body.style.overflow = 'hidden';
 
     }
+    body.addEventListener('input', ()=>{
+        const repl = (e) => {
+			e.value = e.value.replace(/[^0-9]/ig, '');
+			
+        };
+        repl(inputForm);
+        repl(inputForm2);
+        repl(inputPopupFormModal);
+        repl(inputPopupFormModalTell);
+        repl(inputPopupFormModalCalcWidth);
+        repl(inputPopupFormModalCalcHight);
+        repl(inputPopupFormModalCalcEnd);
+        repl(inputForm3);
+    });
 
 //timer
     let deadLine = '2018.12.31';
@@ -98,6 +112,82 @@ closeBtn = addEventListener("click", function (event) {
 		updateClock();
 	};
     setClock('timer', deadLine);
+//form
+    let message = {
+        loading: 'Загрузка...',
+        success: 'Спасибо! Cкоро мы с вами свяжемся',
+        failure: 'Что-то пошло не так'
+    };
+    let form = document.querySelector('form'),
+        popupForm2 = document.querySelectorAll('.form')[1],
+        popupFormModal = document.querySelectorAll('.form')[7],
+        popupForm5 = document.querySelectorAll('.form')[5],
+        popupFormModalTell = document.querySelectorAll('.form')[6],
+        popupFormModalCalcEnd = document.querySelectorAll('.form')[8],
+        inputForm = form.querySelectorAll('input')[1],
+        inputForm2 = document.querySelectorAll('input')[3],
+        inputForm3 = document.querySelectorAll('input')[11],
+        inputPopupFormModal = document.querySelectorAll('input')[15],
+        inputPopupFormModalTell = document.querySelectorAll('input')[13],
+        inputPopupFormModalCalcWidth = document.querySelectorAll('input')[16],
+        inputPopupFormModalCalcHight = document.querySelectorAll('input')[17],
+        inputPopupFormModalCalcEnd = document.querySelectorAll('input')[21],
+        input = document.querySelectorAll('input'),
+        statusMessage = document.createElement('div');
+
+        statusMessage.classList.add('status');
+        
+        function allForm(form) {
+            form.addEventListener('submit', function(e) {
+            e.preventDefault();
+            form.appendChild(statusMessage);
+
+            let formData = new FormData(form);
+            let obj ={};
+            formData.forEach(function(value, key) {
+                obj[key] = value;
+            });
+            let json = JSON.stringify(obj);
+
+            function postData() {
+                return new Promise(function(resolve, reject) {
+                    let request = new XMLHttpRequest();
+                    request.open('POST', 'server.php');
+                    request.setRequestHeader('Content-Type', 'application/json; charset=utf-8');
+                    request.onreadystatechange = function() {
+                        if (request.readyState < 4) {
+                            resolve();
+                        } else if (request.readyState === 4 && request.status == 200){
+                            resolve();
+                        } else {
+                            reject();
+                        }
+                    };
+                    request.send(json);
+                });
+            }
+            function clearInput() {
+                for (let i = 0; i<input.length; i++) {
+                    input[i].value = '';
+                }
+            }
+        postData(formData)
+            .then(()=>statusMessage.innerHTML = message.loading)
+            .then(()=>statusMessage.innerHTML = message.success)
+            .catch(() => statusMessage.innerHTML = message.failure)
+            .then(clearInput);
+        });
+    }
+    function clearInp() {
+        statusMessage.innerHTML = '';
+    }
+    allForm(form);
+    allForm(popupForm2);
+    allForm(popupFormModal);
+    allForm(popupForm5);
+    allForm(popupFormModalTell);
+    allForm(popupFormModalCalcEnd);
+
 
 
 
